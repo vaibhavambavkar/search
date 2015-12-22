49ed21d7f64f17cf4b9f4dabaff5863d19125efa
vaibhavambavkar <vaibhav.ambavkar@vpmmpcoe.org>
2015-12-22 23:08:07 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br', 'root', '26dZNC81L7');
+$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br', 'root', '26dZNC81L7','student');
 
 //if connection is not successful you will see text error
 if (!$link) {
 
        die('Could not connect: ' . mysql_error());
 }
-$db="student";
-mysql_select_db($db); 
+//$db="student";
+//mysql_select_db($db); 
 
 ?>
