5072fa4386292a20b34458387163ad2a0640bd96
vaibhavambavkar <vaibhav.ambavkar@vpmmpcoe.org>
2015-12-22 20:30:08 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br ', 'root', '26dZNC81L7');
+$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com', 'root', '26dZNC81L7');
 //if connection is not successful you will see text error
 if (!$link) {
        $db="student";
