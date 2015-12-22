43dc9019c21260f20998bdc29bebaa579af37954
vaibhavambavkar <vaibhav.ambavkar@vpmmpcoe.org>
2015-12-22 23:29:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br', 'root', '26dZNC81L7','student');
+$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br/index.php?token=a485bdd6ea20bda0b56641e46aff2a0a#PMAURL-5:sql.php?db=student&table=paper&server=1&target=&token=a485bdd6ea20bda0b56641e46aff2a0a', 'root', '26dZNC81L7');
 
 //if connection is not successful you will see text error
 if (!$link) {
