5c1855136f613627718cc9ccf11197c19a15205c
vaibhavambavkar <vaibhav.ambavkar@vpmmpcoe.org>
2015-12-22 20:14:07 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,22 +1,11 @@
 <?php
-
-$host_name = "mysql77757-env-3332420.jelasticlw.com.br ";
-$database = "student"; 
-$username = "root";          
-$password = "26dZNC81L7";          
-
-try 
-{
-
-$dbo = new PDO('mysql:host='.$host_name.';dbname='.$database, $username, $password);
-
-} 
-catch (PDOException $e) 
-
-{
-print "Error!: " . $e->getMessage() . "<br/>";
-die();
+$link = mysql_connect('mysql77757-env-3332420.jelasticlw.com.br ', 'root', '26dZNC81L7');
+//if connection is not successful you will see text error
+if (!$link) {
+       die('Could not connect: ' . mysql_error());
 }
-
-
+//if connection is successfuly you will see message bellow
+echo 'Connected successfully';
+ 
+mysql_close($link);
 ?>
